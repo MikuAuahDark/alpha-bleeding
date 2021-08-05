7c4ef3f213115f0f28105be7c1dfafd465ca329a
Miku AuahDark <lumi.hotaru@gmail.com>
2021-08-05 11:07:06 +0800

    Support msvcbuild.bat for alphatrim.

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,4 +1,5 @@
-cl /nologo /Ox /Ot /EHsc /Febin\alpha-bleeding.exe src\alpha-bleeding.cpp src\png.cpp src\lodepng.cpp
-cl /nologo /Ox /Ot /EHsc /Febin\alpha-remove.exe src\alpha-remove.cpp png.obj lodepng.obj
-cl /nologo /Ox /Ot /EHsc /Febin\alpha-set.exe src\alpha-set.cpp png.obj lodepng.obj
+cl /nologo /Ox /Ot /EHsc /Febin\alpha-bleeding.exe src\alpha-bleeding.cpp src\png.cpp src\lodepng.cpp || exit /b
+cl /nologo /Ox /Ot /EHsc /Febin\alpha-remove.exe src\alpha-remove.cpp png.obj lodepng.obj || exit /b
+cl /nologo /Ox /Ot /EHsc /Febin\alpha-set.exe src\alpha-set.cpp png.obj lodepng.obj || exit /b
+cl /nologo /Ox /Ot /EHsc /Febin\alpha-trim.exe src\alpha-trim.cpp png.obj lodepng.obj || exit /b
 del *.obj
